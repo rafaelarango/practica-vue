a66ce06a7d5ec39d2ea5cde9a20408785d232bc2
rafaelarango <arangoramirezrafael@gmail.com>
2021-07-07 19:24:31 -0500

    imprimir informacion en pantalla

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EjercicioUno/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EjercicioUno from  './components/EjercicioUno.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    EjercicioUno
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/EjercicioUno.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1> Esta es mi informacion: {{ informacion }}</h1>
+    <input type="text" v-model="informacion" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      informacion: "",
+    };
+  },
+};
+</script>
