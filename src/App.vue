53dea8632aa1abb55fc53f1f1a1e5c7191585c1b
rafaelarango <arangoramirezrafael@gmail.com>
2021-07-07 20:09:49 -0500

    Suma de dos valores

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <EjercicioUno/>
+    <EjercicioDos/>
   </div>
 </template>
 
 <script>
 import EjercicioUno from  './components/EjercicioUno.vue';
+import EjercicioDos from  './components/EjercicioDos.vue';
 
 export default {
   components: {
-    EjercicioUno
+    EjercicioUno,
+    EjercicioDos
   }
 }
 
@@ -25,4 +28,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+div{
+  margin-top: 50px;
+}
+
+.EjercicioDos input{
+ background: rgb(61, 198, 223); 
+ color: rgb(219, 37, 128);
+}
+
 </style>
---
+++ src/components/EjercicioDos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="EjercicioDos">
+    <input type="text" v-model.number="numeroUno" />
+    <input type="text" v-model.number="numeroDos" />
+    <h4>Total: {{ sumarNumeros }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numeroUno: 0,
+      numeroDos: 0,
+    };
+  },
+
+  computed: {
+    sumarNumeros() {
+      return this.numeroUno + this.numeroDos;
+    },
+  },
+};
+</script>
